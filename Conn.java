75cd9ddefb3e1c1a1d21f8df050876ad9bff8d49
dev.pritam <150615538+dev-pritam-2005@users.noreply.github.com>
2025-11-04 12:59:02 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,19 @@
+package AirlineManagementSystem;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "Pritam@2005");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
